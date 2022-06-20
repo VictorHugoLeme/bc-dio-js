003bae4f7d34dfd7add7a24d6a702aa0fdcac21e
Victor Hugo <victorhleme.dev@gmail.com>
2022-06-20 10:28:24 -0300

    Added a study about asynchronous Js and API calls with fetch method

---
+++ dominando-js/async/assets/js/script.js
@@ -0,0 +1,34 @@
+var wrapper = document.querySelector('.wrapper');
+var imgDiv = document.createElement('div');
+var avatarTag = document.createElement('img');
+var button = document.getElementById('btn');
+
+
+imgDiv.setAttribute('class', 'img-div');
+
+
+var red = document.getElementById('red');
+
+const url = 'https://api.github.com/users/VictorHugoLeme'
+
+const getUser = async () => {
+    try {
+        const data = await fetch(url);
+        const json = await data.json();
+
+        return json.avatar_url;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const loadImg = async () => {
+    return await getUser().then(function (result) {
+        avatarTag.src = result;
+        button.remove();
+        imgDiv.appendChild(avatarTag);
+        wrapper.appendChild(imgDiv);
+    });
+}
+
+button.addEventListener('click', loadImg);
